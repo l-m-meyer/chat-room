4d81d02714ff126cc34607d9bd1a5cb079448f4c
Lisa Meyer <lisamermey@gmail.com>
2022-07-06 16:01:33 -0400

    Initialize Firebase app

---
+++ src/services/firebase.js
@@ -0,0 +1,12 @@
+import { initalizeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0neU_DLfDDzApm9JFNZM42iOX0baeb-g",
+  authDomain: "chat-room-328a4.firebaseapp.com",
+  projectId: "chat-room-328a4",
+  storageBucket: "chat-room-328a4.appspot.com",
+  messagingSenderId: "872510094641",
+  appId: "1:872510094641:web:79656a74599f025d148410"
+};
+
+const app = initalizeApp(firebaseConfig);
