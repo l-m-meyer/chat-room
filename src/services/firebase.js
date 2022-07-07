d9ba43439e28b9180e4df4ba7437fd1456c6abe3
Lisa Meyer <lisamermey@gmail.com>
2022-07-07 13:40:54 -0400

    Initialize Firestore instance

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD0neU_DLfDDzApm9JFNZM42iOX0baeb-g",
@@ -11,6 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 async function loginWithGoogle() {
   try {
