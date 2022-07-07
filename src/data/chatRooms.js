bf3f0cb99b8cc0de3ed2481e103dfdca5f89451e
Lisa Meyer <lisamermey@gmail.com>
2022-07-07 11:53:51 -0400

    Create  object

---
+++ src/data/chatRooms.js
@@ -0,0 +1,10 @@
+const chatRooms = [
+  { id: 'dogs', title: '🐶 Dogs 🐶' },
+  { id: 'food', title: '🥕 Food 🍉' },
+  { id: 'general', title: '💬 General 💬' },
+  { id: 'news', title: '📰 News 📰' },
+  { id: 'music', title: '🎧 Music 🎧' },
+  { id: 'sports', title: '🎾 Sports 🎾' },
+];
+
+export { chatRooms };
