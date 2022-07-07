86b57c692e1d11bf9a502e69ff1ac2629d96825d
Lisa Meyer <lisamermey@gmail.com>
2022-07-07 11:42:09 -0400

    Replace placeholder code with a title and login button

--- src/components/UnathenticatedApp/index.jsx
+++ src/components/UnathenticatedApp/index.jsx
@@ -1,5 +1,19 @@
+import { useAuth } from '../../hooks/useAuth';
+import './styles.css';
+
 function UnathenticatedApp() {
-  return <div>I'm unathenticated!</div>
+  const { login } = useAuth();
+
+  return (
+    <>
+      <h2>Log in to join a chat room!</h2>
+      <div>
+        <button onClick={login} className="login">
+          Login with Google
+        </button>
+      </div>
+    </>
+  );
 }
 
 export { UnathenticatedApp };
