25d6b0acfdabce3cfa4b796c99a57f4178746421
Lisa Meyer <lisamermey@gmail.com>
2022-07-07 12:52:15 -0400

    Creat  component

--- src/components/ChatRoom/index.jsx
+++ src/components/ChatRoom/index.jsx
@@ -1,5 +1,27 @@
+import { Link, useParams } from 'react-router-dom';
+import { chatRooms } from '../../data/chatRooms';
+import './styles.css';
+
 function ChatRoom() {
-  return <div>Chat room</div>
+  const params = useParams();
+
+  const room = chatRooms.find((room) => room.id === params.id);
+
+  if (!room) {
+    // TODO: 404
+  }
+
+  return (
+    <>
+      <h2>{room.title}</h2>
+      <div>
+        <Link>👈 Back to all rooms</Link>
+      </div>
+      <div className='messages-cointainer'>
+        {/* TODO: populate messages */}
+      </div>
+    </>
+  );
 }
 
 export { ChatRoom };
