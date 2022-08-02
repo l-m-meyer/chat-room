fdf3a123be2d9294749e737db7049daebc3b6e97
Lisa Meyer <lisamermey@gmail.com>
2022-08-02 12:56:35 -0400

    fetches messages and unsubscribes listener on clean up

--- src/hooks/useMessages.js
+++ src/hooks/useMessages.js
@@ -1,9 +1,18 @@
+import { useEffect, useState } from "react";
 import {
   getMessages
 } from "../services/firebase";
 
 function useMessages(roomId) {
+  const [messages, setMessages] = useState([]);
 
+  // fetch messages with getMessages() and unsubscribes the listener when the effect cleans up
+  useEffect(() => {
+    const unsubscribe = getMessages(roomId, setMessages);
+    return unsubscribe;
+  }, [roomId]);
+
+  return messages;
 }
 
 export { useMessages };
