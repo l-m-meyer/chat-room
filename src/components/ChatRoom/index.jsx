51402047e9043d5de52aeb4a896caa5ee27fb330
Lisa Meyer <lisamermey@gmail.com>
2022-08-02 13:50:12 -0400

    Add MessageList component to ChatRoom

--- src/components/ChatRoom/index.jsx
+++ src/components/ChatRoom/index.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from 'react-router-dom';
 import { chatRooms } from '../../data/chatRooms';
 import { MessageInput } from '../MessageInput';
+import { MessageList } from '../MessageList';
 import './styles.css';
 
 function ChatRoom() {
@@ -19,6 +20,7 @@ function ChatRoom() {
         <Link to={'/'}>👈 Back to all rooms</Link>
       </div>
       <div className='messages-container'>
+        <MessageList roomId={room.id} />
         <MessageInput roomId={room.id} />
       </div>
     </>
