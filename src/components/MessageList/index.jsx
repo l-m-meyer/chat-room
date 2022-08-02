6154cc977c87ea492cf9bf4c8f78b36dc22f9774
Lisa Meyer <lisamermey@gmail.com>
2022-08-02 14:13:14 -0400

    takes the roomId as a prop, passes to useMessages, then renders the messages

---
+++ src/components/MessageList/index.jsx
@@ -0,0 +1,48 @@
+import { useLayoutEffect, useRef } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useMessages } from "../../hooks/useMessages";
+import "./styles.css";
+
+function MessageList({ roomId }) {
+  const containerRef = useRef(null);
+  const { user } = useAuth();
+  const messages = useMessages(roomId);
+
+  useLayoutEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  });
+
+  return (
+    <div
+      className="message-list-container"
+      ref={containerRef}
+    >
+      <ul className="message-list">
+        {messages.map((message) => (
+          <Message
+            key={message.id}
+            message={message}
+            isOwnMessage={message.uid === user.uid}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Message({ message, isOwnMessage }) {
+  const { displayName, text } = message;
+
+  return (
+    <li
+      className={['message', isOwnMessage && 'own-message'].join(' ')}
+    >
+      <h4 className="sender">{isOwnMessage ? 'You' : displayName}</h4>
+      <div>{text}</div>
+    </li>
+  );
+}
+
+export { MessageList };
